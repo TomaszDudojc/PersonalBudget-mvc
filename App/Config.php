ec2142542de93bc749bb39935be8cd3aa24058c8
Tomasz Dudojć <tomek.dudojc@gmail.com>
2023-04-05 14:48:23 +0200

    Base signup and login system

---
+++ App/Config.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App;
+
+/**
+ * Application configuration
+ *
+ * PHP version 7.0
+ */
+class Config
+{
+
+    /**
+     * Database host
+     * @var string
+     */
+    const DB_HOST = 'localhost';
+
+    /**
+     * Database name
+     * @var string
+     */
+    const DB_NAME = 'mvclogin';
+
+    /**
+     * Database user
+     * @var string
+     */
+    const DB_USER = 'mvcuser';
+
+    /**
+     * Database password
+     * @var string
+     */
+    const DB_PASSWORD = 'secret';
+
+    /**
+     * Show or hide error messages on screen
+     * @var boolean
+     */
+    const SHOW_ERRORS = true;
+
+    /**
+     * Secret key for hashing
+     * @var boolean
+     */
+    const SECRET_KEY = 'TmV0dZ6o0bsido4g8INinDUnAAztSwwh';
+
+    //
+}
---
+++ App/Mail.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App;
+
+use App\Config;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+//require 'vendor/autoload.php';
+
+/**
+ * Mail
+ *
+ * PHP version 7.0
+ */
+class Mail
+{
+
+    /**
+     * Send a message
+     *
+     * @param string $to Recipient
+     * @param string $subject Subject
+     * @param string $text Text-only content of the message
+     * @param string $html HTML content of the message
+     *
+     * @return mixed
+     */
+    public static function send($to, $subject, $text, $html)
+    {
+        //Create an instance; passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'tomek.dudojc@gmail.com';                     //SMTP username
+            $mail->Password   = 'dohzswevkoqiowog';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom('tomek.dudojc@gmail.com');
+            $mail->addAddress('tomek.dudojc@gmail.com');     //Add a recipient. Name is optional
+            $mail->addReplyTo('tomek.dudojc@gmail.com', 'Information');
+           // $mail->addCC('cc@example.com');
+           // $mail->addBCC('bcc@example.com');
+
+            //Attachments
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = $subject;
+            $mail->Body    = $html;
+            $mail->AltBody = $text;
+
+            $mail->send();
+            echo 'Message has been sent';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+}
