56846f7fc171ff5c241e788bc44dd69c5ffc5d94
Tomasz Dudojć <tomek.dudojc@gmail.com>
2023-04-17 12:58:14 +0200

    added saving of expense without payment methods

---
+++ App/Models/Expenses.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+use \App\Auth;
+use \Core\View;
+use \App\Flash;
+
+class Expenses extends \Core\Model
+{
+    public static function getExpenseCategoriesOfUser()
+    {
+        $sql = "SELECT * FROM expenses_category_assigned_to_users WHERE user_id = :user_id";
+
+        $db = static::getDB();
+		$expenseCategories = $db->prepare($sql);
+        $expenseCategories->bindValue(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);        
+		$expenseCategories->execute();
+
+        return $expenseCategories->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function save()
+    {		
+        $this->amount = filter_input(INPUT_POST, 'amount');
+        $this->date =  filter_input(INPUT_POST, 'date');
+        $idOfExpenseCategory = filter_input(INPUT_POST, 'category');    
+        $this->comment =  filter_input(INPUT_POST, 'comment');      
+       
+        $sql = "INSERT INTO expenses VALUES (NULL, :user_id, :idOfExpenseCategory, :amount, :date, :comment )";    		
+												
+		$db = static::getDB();
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindValue(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
+        $stmt->bindValue(':idOfExpenseCategory',  $idOfExpenseCategory, PDO::PARAM_INT);
+        $stmt->bindValue(':amount', $this->amount, PDO::PARAM_STR);
+        $stmt->bindValue(':date', $this->date, PDO::PARAM_STR);
+        $stmt->bindValue(':comment', $this->comment, PDO::PARAM_STR);
+
+        return $stmt->execute();        
+    }	
+}
