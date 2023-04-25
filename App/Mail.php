c62bb7308dbc875ef2c207fdc1fc27ef2e7fc44c
Tomasz Dudojć <tomek.dudojc@gmail.com>
2023-04-25 13:45:11 +0200

    improved views

--- App/Mail.php
+++ App/Mail.php
@@ -37,23 +37,24 @@ class Mail
             //Server settings
             //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
             $mail->isSMTP();                                              //Send using SMTP
-            $mail->Host       = '...';                         //Set the SMTP server to send through
+            $mail->Host       = 'smtp.gmail.com';                         //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                     //Enable SMTP authentication
-            $mail->Username   = '...';                 //SMTP username
-            $mail->Password   = '...';                       //SMTP password
+            $mail->Username   = 'tomek.dudojc@gmail.com';                 //SMTP username
+            $mail->Password   = 'vwnqejkzgxurpury';                       //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;              //Enable implicit TLS encryption
             $mail->Port       = 465;                                      //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
-            $mail->setFrom('...');
+            $mail->setFrom('tomek.dudojc@gmail.com');
             $mail->addAddress($to);                                       //Add a recipient. Name is optional
-            $mail->addReplyTo('...', 'Information');
+            $mail->addReplyTo('tomek.dudojc@gmail.com', 'Information');
             //$mail->addCC('cc@example.com');
             //$mail->addBCC('bcc@example.com');
 
             //Attachments
             //$mail->addAttachment('/var/tmp/file.tar.gz');               //Add attachments
             //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');          //Optional name            
+           
            
             //Content
             $mail->isHTML(true);                                          //Set email format to HTML
