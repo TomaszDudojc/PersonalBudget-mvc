9e09fd17697ec8f67225e1ff44f6c5a3d82a25eb
Tomasz Dudojć <tomek.dudojc@gmail.com>
2023-04-25 13:49:29 +0200

    correcet mail.php

--- App/Mail.php
+++ App/Mail.php
@@ -37,17 +37,17 @@ class Mail
             //Server settings
             //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
             $mail->isSMTP();                                              //Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                         //Set the SMTP server to send through
+            $mail->Host       = '...';                         //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                     //Enable SMTP authentication
-            $mail->Username   = 'tomek.dudojc@gmail.com';                 //SMTP username
-            $mail->Password   = 'vwnqejkzgxurpury';                       //SMTP password
+            $mail->Username   = '...';                 //SMTP username
+            $mail->Password   = '...';                       //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;              //Enable implicit TLS encryption
             $mail->Port       = 465;                                      //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
-            $mail->setFrom('tomek.dudojc@gmail.com');
+            $mail->setFrom('...');
             $mail->addAddress($to);                                       //Add a recipient. Name is optional
-            $mail->addReplyTo('tomek.dudojc@gmail.com', 'Information');
+            $mail->addReplyTo('...', 'Information');
             //$mail->addCC('cc@example.com');
             //$mail->addBCC('bcc@example.com');
 
